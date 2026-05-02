28f2a5b616a1e32515239dedd665a80577a07202
Tomas Carnecky <tomc@caurea.org>
2026-05-02 14:38:15 +0200

    🍻

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix-develop.url = "github:nicknovitski/nix-develop";
+    nix-develop.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      nix-develop,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        packages.nix-develop = nix-develop.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+        };
+
+        devShells.workflow = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+        };
+      }
+    );
+}
